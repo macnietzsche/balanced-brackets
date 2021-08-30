40a175df0f76b0c90dc2b99275e23341e9092da1
mvillegas <mbvillegas94@gmail.com>
2021-08-30 17:00:29 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+CLOSING_PAIRS={
+    "(":")",
+    "[":"]",
+    "{":"}"
+}
+
+def getValueByIndex(arr,index):
+    if(index not in arr):
+        return None
+    return arr[index]
+
+def getLastElement(arr):
+    size=len(arr);
+    if size<=0:
+        return None
+    return arr[size-1]
+
+def isBalanced(exp):
+    stack=[]
+    for element in exp:
+        closing=getValueByIndex(CLOSING_PAIRS,getLastElement(stack))
+        if(element==closing):
+            stack.pop()
+        elif(element in CLOSING_PAIRS.values() and element!=closing):
+            return False
+        elif(element in CLOSING_PAIRS or element in CLOSING_PAIRS.values()):
+            stack.append(element)
+    return len(stack)==0
+
+str="([{fghg}]){()}[]"
+print(isBalanced(str))
+    
